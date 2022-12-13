16ee9e065910d6cf08a159754e8186c004ca7120
Tom Boettger <t.boettger@live.de>
2022-12-13 01:59:06 +0100

    Print help instead of panicing when no file provided

--- cmd/mdfc.go
+++ cmd/mdfc.go
@@ -7,6 +7,29 @@ import (
 	"strconv"
 )
 
+func printHelp() {
+	fmt.Println("Usage: mdfc [options] [file]")
+	fmt.Println("\nOptions:")
+	fmt.Println("\n\t-h, --help")
+	fmt.Println("\t\tShow this help message and exit.")
+	fmt.Println("\n\t-s, --sequential")
+	fmt.Println("\t\tShow flashcards in sequential order as in the markdown file. The default behavior is to")
+	fmt.Println("\t\tshow flashcards in random order.")
+	fmt.Println("\n\t-c, --category <category>")
+	fmt.Println("\t\tShow only flashcards of the specified category. If no category is specified, you can")
+	fmt.Println("\t\tinteractively choose one.")
+	fmt.Println("\t\tIt is possible to specify the category by a chapter number or the categories first word,")
+	fmt.Println("\t\te.g. \"2.1\" for \"2.1 Regular Expressions\".")
+	fmt.Println("\n\t-o, --show-category")
+	fmt.Println("\t\tShow the category of each flashcard.")
+	fmt.Println("\n\t-t, --test <number_flashcards>")
+	fmt.Println("\t\tTest yourself in test mode with random flashcards. If no number is specified, all")
+	fmt.Println("\t\tflashcards will be tested.")
+	fmt.Println("\n\t-n, --number <number_flashcards>")
+	fmt.Println("\t\tLearn n cards during the session. If no number is specified, it will fall back to the")
+	fmt.Println("\t\tnumber specified in the YAML front matter of the markdown file. Defaults to 20.")
+}
+
 const defaultNumberCards = 20
 
 func main() {
@@ -17,26 +40,7 @@ func main() {
 	for i, arg := range args {
 		switch arg {
 		case "-h", "--help":
-			fmt.Println("Usage: mdfc [options] [file]")
-			fmt.Println("\nOptions:")
-			fmt.Println("\n\t-h, --help")
-			fmt.Println("\t\tShow this help message and exit.")
-			fmt.Println("\n\t-s, --sequential")
-			fmt.Println("\t\tShow flashcards in sequential order as in the markdown file. The default behavior is to")
-			fmt.Println("\t\tshow flashcards in random order.")
-			fmt.Println("\n\t-c, --category <category>")
-			fmt.Println("\t\tShow only flashcards of the specified category. If no category is specified, you can")
-			fmt.Println("\t\tinteractively choose one.")
-			fmt.Println("\t\tIt is possible to specify the category by a chapter number or the categories first word,")
-			fmt.Println("\t\te.g. \"2.1\" for \"2.1 Regular Expressions\".")
-			fmt.Println("\n\t-o, --show-category")
-			fmt.Println("\t\tShow the category of each flashcard.")
-			fmt.Println("\n\t-t, --test <number_flashcards>")
-			fmt.Println("\t\tTest yourself in test mode with random flashcards. If no number is specified, all")
-			fmt.Println("\t\tflashcards will be tested.")
-			fmt.Println("\n\t-n, --number <number_flashcards>")
-			fmt.Println("\t\tLearn n cards during the session. If no number is specified, it will fall back to the")
-			fmt.Println("\t\tnumber specified in the YAML front matter of the markdown file. Defaults to 20.")
+			printHelp()
 			return
 		case "-s", "--sequential":
 			session.Sequential = true
@@ -75,7 +79,8 @@ func main() {
 
 	err := session.ReadFile()
 	if err != nil {
-		panic(err)
+		printHelp()
+		return
 	}
 
 	if os.Getenv("DEBUG") == "true" {
